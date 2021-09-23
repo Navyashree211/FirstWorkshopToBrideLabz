da56e6ea265e828d53c7e9f090bfd8326048f0c9
navyas211@gmail.com <navyas211@gmail.com>
2021-09-23 22:29:10 +0530

    [NAVYA]ADDED: This branch Show board display the current board

--- src/com/BridgeLabz/Utility/Utility.java
+++ src/com/BridgeLabz/Utility/Utility.java
@@ -2,9 +2,9 @@ package com.BridgeLabz.Utility;
 
 public class Utility {
 	// board case
-	public static void boardCase() {
+	public static void boardCase(char[] mat) {
 
-		char mat[] = new char[10];
+		//char mat[] = new char[10];
 		for (int i = 1; i < 10; i++) {
 			mat[i] = ' ';
 		}
@@ -27,4 +27,16 @@ public class Utility {
 		}
 	}
   }
+	//A player would like to see the board so
+	//I can choose valid cell to make my move during my turn..
+	//This method show board display the current board..
+	public static void showBoard(char[] mat) {
+    System.out.println("| "+mat[1]+"|"+mat[2]+"|"+mat[3]+" |");
+    System.out.println("|-------|");
+    System.out.println("| "+mat[1]+"|"+mat[2]+"|"+mat[3]+" |");
+    System.out.println("|-------|");
+    System.out.println("| "+mat[1]+"|"+mat[2]+"|"+mat[3]+" |");
+    
+		
+	}
 }
--- src/com/BridgeLabz/Workshop/TicTacGame.java
+++ src/com/BridgeLabz/Workshop/TicTacGame.java
@@ -3,15 +3,17 @@ package com.BridgeLabz.Workshop;
 import com.BridgeLabz.Utility.Utility;
 
 public class TicTacGame {
+	static char mat[] = new char[10];
+
 	public static void main(String[] args) {
 		System.out.println("Tic-Tac game started");
 
 		System.out.println("user :");
 		String user = InputUtility.getString();
-
 		Utility.getTicTacGame(user);
 
-		Utility.boardCase();
+		Utility.boardCase(mat);
+		Utility.showBoard(mat);
 	}
 
 }
