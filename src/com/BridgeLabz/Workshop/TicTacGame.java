5e200147d80ff546ae1c5d7c79c0fc1467bbd5e3
navyas211@gmail.com <navyas211@gmail.com>
2021-09-23 09:49:18 +0530

    [Navya]Added: PlayGame

--- src/com/BridgeLabz/Workshop/TicTacGame.java
+++ src/com/BridgeLabz/Workshop/TicTacGame.java
@@ -1,5 +1,7 @@
 package com.BridgeLabz.Workshop;
 
+import java.util.Random;
+
 public class TicTacGame {
 	public static void main(String[] args) {
 		System.out.println("Tic-Tac game started");
@@ -12,6 +14,23 @@ public class TicTacGame {
 		for (int i = 1; i < 10; i++) {
 			mat[i] = ' ';
 		}
+	}
+	
+	public void PlayCom(char []t)
+	{
+		System.out.println("Computer Term to play the game");
+		Random rd = new Random();
+		int r=rd.nextInt(t.length);
+		System.out.println(r);
+		
+		if (t[r]==0)
+		{
+			t[r] = 'X';
+		}
+		else {
+			System.out.println("Invalid Input");
+		}
+		
 	}
 
 }
