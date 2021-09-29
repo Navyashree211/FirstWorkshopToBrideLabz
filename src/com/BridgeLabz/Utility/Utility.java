d74e5a4fd1b1b7d0748d31b6f54b988cab883748
navyas211@gmail.com <navyas211@gmail.com>
2021-09-29 17:56:44 +0530

    [NAVYA]ADDED: Ability for User to make a move desired location

--- src/com/BridgeLabz/Utility/Utility.java
+++ src/com/BridgeLabz/Utility/Utility.java
@@ -12,24 +12,19 @@ public class Utility {
 
 	// Play game to come
 	public static void getTicTacGame(char user) {
-		//System.out.println(">"+user);
-		//for (int i = 0; i < user.length(); i++) {
-			
-			char ch = user;
-			//System.out.println("> >"+ch);
-			if (ch == 'X' || ch == 'O' || ch == 'x' || ch == 'o') {
+		char ch = user;
+		if (ch == 'X' || ch == 'O' || ch == 'x' || ch == 'o') {
 
-				System.out.println("Choice your charctere is : " + ch);
+			System.out.println("Choice your charctere is : " + ch);
 
-				char computer = (ch == 'X') ? 'O' : 'X'; // here we can use ternary operator
-															// (condition ? expr1 : expr2)
-			} // if the condition is true then execute expr1 and condition is false then
-				// execute expr2..
-			else {
-				System.out.println("Wrong choice: ");
-			} 
+			char computer = (ch == 'X') ? 'O' : 'X'; // here we can use ternary operator
+														// (condition ? expr1 : expr2)
+		} // if the condition is true then execute expr1 and condition is false then
+			// execute expr2..
+		else {
+			System.out.println("Wrong choice: ");
 		}
-	
+	}
 
 	// A player would like to see the board so
 	// I can choose valid cell to make my move during my turn..
@@ -45,42 +40,9 @@ public class Utility {
 
 	// Ability for user to make a move to a desired location in the board.
 
-	public static void UserInput(char user, int possion, char [] mat) {
+	public static void UserInput(char user, int possion, char[] mat) {
 		System.out.println(possion);
 		mat[possion] = user;
-		//System.out.println(mat[possion]);
-		
+
 	}
 }
-//		switch (possion) {
-//		case 1:
-//			mat[possion] = 'X';
-//			break;
-//		case 2:
-//			mat[possion] = 'X';
-//			break;
-//		case 3:
-//			mat[possion] = 'X';
-//			break;
-//		case 4:
-//			mat[possion] = 'X';
-//			break;
-//		case 5:
-//			mat[possion] = 'X';
-//			break;
-//		case 6:
-//			mat[possion] = 'X';
-//			break;
-//		case 7:
-//			mat[possion] = 'X';
-//			break;
-//		case 8:
-//			mat[possion] = 'X';
-//			break;
-//		case 9:
-//			mat[possion] = 'X';
-//			break;
-//		}
-//		
-//	}
-//}
